15c1f7d335256fe2d0a7d9f9eec04abc6ae9c9dc
ChauDinh <chaudvb95uc@gmail.com>
2019-01-15 23:22:29 +0700

    connect to mongo

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require("./src/app");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://0.0.0.0:27017/fmail", {
+mongoose.connect("mongodb://0.0.0.0:27017/fmail", {
  useNewUrlParser: true
-});
+})
+ .then(() => console.log("Connected to MongoDB..."))
+ .catch(err => console.error(err));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+
+process.on("unhandledRejection", e => {
+ console.log(e.message);
+ process.exit(1);
+});
